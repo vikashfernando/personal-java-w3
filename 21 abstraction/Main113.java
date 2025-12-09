9841075e56eadd432f96e22e40e353c205706b9c
vikashfernando <vikashdeshitha@gmail.com>
2025-12-09 17:04:55 +0530

    update

---
+++ 21 abstraction/Main113.java
@@ -0,0 +1,33 @@
+/*
+Data abstraction is the process of hiding certain details and showing only essential information to the user.
+Abstraction can be achieved with either abstract classes or "interfaces" (which you will learn more about in the next chapter).
+The abstract keyword is a non-access modifier, used for classes and methods:
+    Abstract class: is a restricted class that cannot be used to create objects (to access it, it must be inherited from another class).
+    Abstract method: can only be used in an abstract class, and it does not have a body. ***The body is provided by the subclass (inherited from).***
+
+ */
+
+
+abstract class Animal1{              //an abstract class
+    abstract void animalSound();        //an abstract method
+
+    public void sleep(){                //a normal method
+        System.out.println("Zzz...");
+    }
+}
+
+class Dog1 extends Animal1{         //a subclass of Animal class
+    void animalSound(){
+        System.out.println("bowww"); //*** ***
+    }
+}
+
+public class Main113{
+    public static void main(String[] args){
+        Dog1 o1=new Dog1();
+
+        o1.sleep();
+        o1.animalSound();
+
+    }
+}
